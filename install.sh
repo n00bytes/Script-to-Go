b0bd3562ee5aa4d7bf1f77ec2cb8def3bf6f3f9f
n00bytes <82503062+n00bytes@users.noreply.github.com>
2022-04-13 13:51:58 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+if ! command -v go &>/dev/null; then
+wget https://go.dev/dl/go1.18.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz && cp /usr/local/go/bin/go /usr/local/bin/
+else
+	echo "Go Already Installed"
+fi
+
+if ! command -v amass &>/dev/null; then
+go install -v github.com/OWASP/Amass/v3/...@master
+else
+        echo "Amass Already Installed"
+fi
+if ! command -v subfinder &>/dev/null; then
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest && sleep 10 && cp /root/go/bin/subfinder /usr/local/bin/
+else
+        echo "Subfinder Already Installed"
+fi
+if ! command -v aquatone &>/dev/null; then
+wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -O /tmp/aquatone_linux_amd64.zip && unzip -a /tmp/aquatone_linux_amd64.zip -d /tmp && cp /tmp/aquatone /usr/local/bin
+else
+        echo "Aquatone Already Installed"
+fi
+if ! command -v httpx &>/dev/null; then
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest && sleep 10 && cp /root/go/bin/httpx /usr/local/bin/
+else
+        echo "HTTPx Already Installed"
+fi
+if ! command -v nuclei &>/dev/null; then
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest && sleep 10 && cp /root/go/bin/nuclei /usr/local/bin/
+else
+        echo "Nuclei Already Installed"
+fi
+if ! command -v dnsx &>/dev/null; then
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest  && sleep 10 && cp /root/go/bin/dnsx /usr/local/bin/
+else
+        echo "Dnsx Already Installed"
+fi
+
