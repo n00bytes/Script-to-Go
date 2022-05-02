5696291bfd836920409779449b9f1882a820c577
n00bytes <82503062+n00bytes@users.noreply.github.com>
2022-05-03 01:53:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+apt update
+
 if ! command -v go &>/dev/null; then
 wget https://go.dev/dl/go1.18.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz && cp /usr/local/go/bin/go /usr/local/bin/
 else
@@ -19,6 +22,12 @@ if ! command -v aquatone &>/dev/null; then
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -O /tmp/aquatone_linux_amd64.zip && unzip -a /tmp/aquatone_linux_amd64.zip -d /tmp && cp /tmp/aquatone /usr/local/bin
 else
         echo "Aquatone Already Installed"
+	
+if ! command -v chromium &>/dev/null; then
+sudo apt install chromium -y
+else
+        echo "Chromium Already Installed"
+
 fi
 if ! command -v httpx &>/dev/null; then
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest && cp /root/go/bin/httpx /usr/local/bin/
