e1dfc856832cb9c6ca5e91e19985e431c672a50a
n00bytes <82503062+n00bytes@users.noreply.github.com>
2022-09-23 14:15:57 +0800

    Update Script2Go.sh

--- Script2Go.sh
+++ Script2Go.sh
@@ -17,25 +17,30 @@ cat << "EOF"
 ###################################################################################                                                                                    
 EOF
 echo ""
-usage() { echo "Usage: $0 [-h <usage>] [-f <Target File>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-h <usage>] [-f <Target File>] [-d <Domain>]" 1>&2; exit 1; }
 
-while getopts ":h:f:" o; do
-    case "${o}" in
+while getopts "h:f:d:" opt; do
+    case "${opt}" in
         h)
             h=${OPTARG}
             usage
-	    exit;;
+	    exit
+	    ;;
         f)
             f=${OPTARG}
             ;;
+	d)
+	    d=${OPTARG}
+	    ;;
         *)
             usage
-            exit;;
+	    exit
+	    ;;
     esac
 done
 shift $((OPTIND-1))
 
-if [ ! -f "${f}" ]; then
+if [ ! -f "${f}" ] && [  "${d}" == "" ]; then
     usage
 fi
 
@@ -45,7 +50,7 @@ BGreen='\033[1;32m'       # Green
 BYellow='\033[1;33m'      # Yellow
 BBlue='\033[1;34m'	  # Blue
 
-TargetFile=${f}
+targetFile=${f}
 echo ""
 echo -e "${BGreen}Go Grab Some Coffee........"
 echo ""
@@ -55,26 +60,37 @@ outAquatone="$outDir"/Aquatone-$(date +"%d-%m-%Y")
 outNmap="$outDir"/NmapResult$(date +"%d-%m-%Y")
 mkdir "$outDir" "$outNmap" "$outAquatone"
 
+AmassConfig=/home/consultant/Tools/config.ini    #Edit path to your config.ini
+SubfinderConfig=/root/.config/subfinder/provider-config.yaml	#Edit path to your provider-config.yaml
+
 #### Comment out if you want to use config file for Amass and Subfinder ###
 #AmassConfig=/Path/config.ini    #Uncomment and edit path to your config.ini
 #SubfinderConfig=/Path/provider-config.yaml	#Uncomment and edit path to your provider-config.yaml
 
+### Nmap Top ports TCP ###
+#nmap -T4 -Pn -n -sS -A --open --top-ports 1000 --min-rate 500 --max-rate 1000 --max-retries 3 --defeat-rst-ratelimit -iL $targetFile -oA $outNmap/Nmap_TopPorts_TCP &>/dev/null
+echo -e "${BGreen}Port Scanning Top-ports 1000 TCP........${BRed}[DONE]"
 
-nmap -T4 -Pn -n -sS -A --open --top-ports 10000 --min-rate 500 --max-rate 1000 --max-retries 2 --defeat-rst-ratelimit -iL $TargetFile -oA $outNmap/nmap_top_10k
-echo -e "${BGreen}Port Scanning Top-ports 10000 TCP........${BRed}[DONE]"
+### Nmap Top ports UDP ###
+#nmap -T4 -Pn -n -sU -A --open --top-ports 200 --min-rate 500 --max-rate 1000 --max-retries 3 --defeat-rst-ratelimit -iL $targetFile -oA $outNmap/Nmap_TopPorts_UDP &>/dev/null
+echo -e "${BGreen}Port Scanning Top-ports 200 UDP........${BRed}[DONE]"
 
+#cat $outNmap/nmap_top_10k.nmap | grep 'commonName' | awk '{print $4}' | awk -F '=|/' '{print $2}' | rev | cut -d "." -f1-2 | rev | sort -u | awk -F '.' 'NF>1' > $outDir/domainLists.txt
+#echo -e "${BGreen}Extracting domain list........${BRed}[DONE]"
 
-cat $outNmap/nmap_top_10k.nmap | grep 'commonName' | awk '{print $4}' | awk -F '=|/' '{print $2}' | rev | cut -d "." -f1-2 | rev | sort -u | awk -F '.' 'NF>1' > $outDir/domainLists.txt
-echo -e "${BGreen}Extracting domain list........${BRed}[DONE]"
+if [  "$d" == "" ]; then
 
-if [ -s $outDir/domainLists.txt ]; then
+	# If no domain names found.
+	echo -e "${BBlue}No provided domain ..Skipping Subdomain enumeration..${BRed}[SKIP]"
         # If domain names found.
-       	amass enum -silent -df $outDir/domainLists.txt -o $outDir/AmassOut.txt &>/dev/null
-	#amass enum -silent -df "$outDir"/domainLists.txt -c $AmassConfig -o "$outDir"/AmassOut.txt #uncomment out this line and comment the line above if you have config file.
+       	
+else
+	#amass enum -silent -df $outDir/domainLists.txt -o $outDir/AmassOut.txt &>/dev/null
+	amass enum -silent -d $d -config $AmassConfig -o $outDir/AmassOut.txt #uncomment out this line and comment the line above if you have config file.
 	echo -e "${BGreen}Amass scanning......${BRed}[DONE]"
 
-	subfinder -silent -dL $outDir/domainLists.txt -o $outDir/SubfinderOut.txt &>/dev/null
-	#subfinder  -silent -dL "$outDir"/domainLists.txt -c $SubfinderConfig -o "$outDir"/SubfinderOut.txt &>/dev/null #uncomment out this line and comment line above if you have config file.
+	#subfinder -silent -dL $outDir/domainLists.txt -o $outDir/SubfinderOut.txt &>/dev/null
+	subfinder  -silent -d $d -config $SubfinderConfig -o $outDir/SubfinderOut.txt &>/dev/null #uncomment out this line and comment line above if you have config file.
 	echo -e "${BGreen}Subfinder scanning......${BRed}[DONE]"
 
 	cat $outDir/AmassOut.txt $outDir/SubfinderOut.txt | sort -u > $outDir/Subdomains.txt
@@ -82,9 +98,6 @@ if [ -s $outDir/domainLists.txt ]; then
 
 	cat $outDir/Subdomains.txt | dnsx -silent -resp -a -aaaa -o $outDir/DnsxOut.txt &>/dev/null
 	echo -e "${BGreen}Performing reverse dns queries......${BRed}[DONE]"
-else
-        # If no domain names found.
-	echo -e "${BBlue}No extract domain from Nmap..Skipping Subdomain enumeration..${BRed}[SKIP]"
 fi
 
 if [ -f $outDir/DnsxOut.txt ]; then
@@ -100,7 +113,7 @@ if [ -f $outDir/In-ScopeSubdomains.txt ]; then
 	echo -e "${BGreen}Running Aquatone........${BRed}[DONE]"
 else
 	cat $TargetFile | aquatone -ports large -out $outAquatone/ &>/dev/null
-	echo -e "${BGreen}Running Aquatone........${BRed}[DONE]"
+	echo -e "${BGreen}Skipping Aquatone........${BRed}[SKIP]"
 fi
 
 cat $outAquatone/aquatone_urls.txt | awk {'print $1'} | httpx -status-code -title -tech-detect -o $outDir/HTTPxOut.txt &>/dev/null
